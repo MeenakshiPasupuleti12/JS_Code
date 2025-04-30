b4c1169aeca7ad0c56123be57f037f78d6be5b8d
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2025-04-30 21:58:21 +0530

    add the code into repo

--- var.py
+++ var.py
@@ -1,5 +1,17 @@
-n = 5
+# n = 5
 
-for i in range(1, 5):
-    for j in range(i, 5):
-        print(f"*{i}*{j}*")
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(f"*{i}*{j}*")
+
+
+
+# ---------------
+marks = 85
+if marks >= 90:
+ print("Grade: A")
+Python Notes 24
+elif marks >= 75:
+ print("Grade: B")
+elif marks >= 50:
+ print("Grade: C")
